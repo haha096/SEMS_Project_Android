089063edcd9306fac9b9aa7a21a5e68b47680fbc
haha096 <lucky-96@naver.com>
2025-10-07 19:22:10 +0900

    안드로이드 연동

--- lib/src/service/api_client.dart
+++ lib/src/service/api_client.dart
@@ -12,7 +12,8 @@ class ApiClient {
     if (_dio != null) return _dio!;
 
     final dio = Dio(BaseOptions(
-      baseUrl: "http://10.0.2.2:8080",
+      //baseUrl: "http://10.0.2.2:8080",
+      baseUrl : "http://127.0.0.1:8080",
       connectTimeout: const Duration(seconds: 5),
       receiveTimeout: const Duration(seconds: 10),
       validateStatus: (_) => true,
